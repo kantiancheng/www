c14584243a24f0d0e2bc29c9c2381d798b5b74a5
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-06-17 20:30:50 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+beforeLOAD();
+var uname;
+
+//侧导航栏颜色变化
+function overr(x) {
+    x.style.backgroundColor = "rgb(119, 146, 255)";
+}
+
+function leaver(x) {
+    x.style.backgroundColor = "rgb(67, 105, 255)";
+}
+
+function beforeLOAD() {
+    //用户登录验证
+    if (localStorage.getItem('cemail') == null || localStorage.getItem('userpass') == null || localStorage.getItem('cemail') == "" || localStorage.getItem('userpass') == "") {
+        window.location = "./login.html";
+    }
+}
+
+function loade() {
+    uname = localStorage.getItem('uname');
+    document.getElementById("uname").innerHTML = uname;
+}
