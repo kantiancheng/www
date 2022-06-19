bd91837b09754532fbf9977826ff2ffbb72682a9
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-06-19 16:19:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 beforeLOAD();
 var languagee;
 var uname;
+var time_w;
+var pagee;
 
 //侧导航栏颜色变化
 function overr(x) {
@@ -14,13 +16,19 @@ function leaver(x) {
 function beforeLOAD() {
     //用户登录验证
     if (localStorage.getItem('cemail') == null || localStorage.getItem('userpass') == null || localStorage.getItem('cemail') == "" || localStorage.getItem('userpass') == "") {
-        window.location = "./login.html";
+        if (pagee != "no") {
+            window.location = "./login.html";
+
+        }
     }
 }
 
 function loadee() {
-    uname = localStorage.getItem('uname');
-    document.getElementById("uname").innerHTML = uname;
+    if (pagee == "no") {} else {
+        uname = localStorage.getItem('uname');
+        document.getElementById("uname").innerHTML = uname;
+    }
+    document.getElementById("inform").hidden = true;
     languagechenge();
 }
 
@@ -41,10 +49,33 @@ function language() {
     if (languagee == "EN") {
         document.getElementById("language").innerHTML = "English";
         localStorage.setItem("languagee", "CN");
+        inform("语言更改为中文", 4000);
     }
     if (languagee == "CN") {
         document.getElementById("language").innerHTML = "中文";
         localStorage.setItem("languagee", "EN");
+        inform("Language changed to English", 4000);
     }
     languagechenge();
+}
+
+function inform(text, time) {
+    if (document.getElementById("inform_in").innerHTML == "" || document.getElementById("inform_in").innerHTML == null) {
+        time_w = time;
+        in_te = text;
+        document.getElementById("inform").hidden = false;
+        document.getElementById("inform_in").innerHTML = text;
+        setTimeout(function() {
+            document.getElementById("inform_in").innerHTML = "";
+            document.getElementById("inform").hidden = true;
+        }, time);
+    } else {
+        if (in_te != text) {
+            setTimeout(function() {
+                inform(text, time);
+            }, time_w);
+        }
+        time_w = time;
+        in_te = text;
+    }
 }
